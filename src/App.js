23a16b85958f369ceadab2399a93f56d4a5a97ff
reedoooo <readvogt@gmail.com>
2023-02-13 21:20:31 -0800

    lab complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Appfooter from './components/Appfooter';
+import Appheader from './components/Appheader';
+import Appmain from './components/Appmain';
+import style from './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Appheader />
       </header>
+      <main className="App-main">
+          <Appmain />
+      </main>
+      <footer className="App-footer">
+          <Appfooter />
+      </footer>
     </div>
   );
 }
---
+++ src/components/Appfooter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Appfooter extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Reed Vogt</p>
+            </div>
+        )
+    }
+}
+
+export default Appfooter;
